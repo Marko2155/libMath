d7a2ce1f56b2d7251a3c65974417a708468abaf5
Marko2155 <98662057+Marko2155@users.noreply.github.com>
2023-02-22 13:42:21 +0100

    Create __init__.py

---
+++ libmath/__init__.py
@@ -0,0 +1,25 @@
+# libMath by Marko2155.
+# Please do not remove these comments, modify any code or reupload this to PyPI without crediting the owner first.
+
+def add(num1add, num2add):
+    return num1add + num2add
+
+def take(num1take, num2take):
+    return num1take - num2take
+
+def divide(num1divide, num2divide):
+    return num1divide / num2divide
+    
+def multiply(num1multiply, num2multiply):
+    return num1multiply * num2multiply
+
+def greaterlowerorequalto(num1oget, num2oget):
+    if num1oget > num2oget:
+        return 1
+    if num1oget < num2oget:
+        return 0
+    if num1oget == num2oget:
+        return 2
+
+pi = 3.141592653589793
+e = 2.718281828459045
